8b55f15f73d85589f47b528e0bd975c0da68c38f
Binita1225 <Binita1225@users.noreply.github.com>
2024-11-25 12:22:04 +0545

    company added and crud performed

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace BulkyBook.DataAccess.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Company> Companies { get; set; }
         
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
          public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
@@ -27,6 +28,37 @@ namespace BulkyBook.DataAccess.Data
                 new Category { Id =1, Name = "Action", DisplayOrder = 1},
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Company>().HasData(
+                new Company { 
+                    Id = 1, 
+                    Name = "Tech Solution",
+                    StreetAddress = "123 Tech St",
+                    City="Tech City",
+                    PostalCode="12121",
+                    State="IL",
+                    PhoneNumber="9800000000"},
+                new Company
+                {
+                    Id = 2,
+                    Name = "Vivid Books",
+                    StreetAddress = "999 Vid St",
+                    City = "Vid City",
+                    PostalCode = "66666",
+                    State = "IL",
+                    PhoneNumber = "9801110000"
+                },
+                new Company
+                {
+                    Id = 3,
+                    Name = "Readers Club",
+                    StreetAddress = "999 Main St",
+                    City = "Lala land",
+                    PostalCode = "99999",
+                    State = "NY",
+                    PhoneNumber = "9800222000"
+                }
                 );
 
             modelBuilder.Entity<Product>().HasData(
