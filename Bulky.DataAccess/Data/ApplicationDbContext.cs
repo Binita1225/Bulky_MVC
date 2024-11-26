5b9dea961ca42c601880ca2de50834fb96070096
Binita1225 <Binita1225@users.noreply.github.com>
2024-11-26 15:31:58 +0545

    order details & summary

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,9 @@ namespace BulkyBook.DataAccess.Data
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
          public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
